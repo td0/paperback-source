1702f01ac94d99dcd5f405e766c22f95ed66bc27
Tadho <muhammad.murtadho@dana.id>
2021-11-10 13:28:46 +0700

    feat: add helpers
    Signed-off-by: Tadho <muhammad.murtadho@dana.id>

--- src/MangaSail/MangaSailHelper.ts
+++ src/MangaSail/MangaSailHelper.ts
@@ -1,11 +1,55 @@
+// Manga Data
 export const NAME = 'MangaSail'
 export const BASE_DOMAIN = 'https://www.mangasail.co'
+export const MANGA_DETAILS_PATH = `${BASE_DOMAIN}/content`
+export const MANGA_DETAILS_FIELDS = [
+  'field_image2',
+  'field_status',
+  'field_artist',
+  'field_author',
+  'field_genres',
+  'body',
+]
+export const INTERCEPT_SEARCH_IMG = 'search_image_path'
+export const HEADERS = { 'X-Authcache': '1' }
+export const HEADER_REF_SEARCH_KEY = 'X-ref-search'
+
+// Source Data
 export const VERSION = '0.0.1'
 export const METHOD = 'GET'
 export const DESCRIPTION = 'td0\'s extension for paperback'
 export const AUTHOR = 'td0'
 export const HOMEPAGE = 'https://github.com/td0/'
-export const TITLE_THUMBNAIL_PATH = 'title_thumbnail_portrait_list'
-export const HEADERS = {
-  'X-Authcache': '1'
+
+export const mangaDetailFieldsMapper = (
+  results: string[]
+): Record<string, unknown> => {
+  return results.reduce((
+    result: Record<string, string>,
+    field,
+    idx
+  ) => {
+    let fieldKey
+    switch(MANGA_DETAILS_FIELDS[idx]) {
+      case 'field_image2':
+        fieldKey = 'image'
+        break
+      case 'field_status':
+        fieldKey = 'status'
+        break
+      case 'field_author':
+        fieldKey = 'author'
+        break
+      case 'field_artist':
+        fieldKey = 'artist'
+        break
+      case 'body':
+        fieldKey = 'desc'
+        break
+      default:
+        fieldKey = MANGA_DETAILS_FIELDS[idx] as string
+    }
+    result[fieldKey] = field
+    return result
+  }, {})
 }
